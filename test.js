0c25eebecdfba444ecb0c40a619bd6da9b3d9045
Adekunle <adekunleabioye27@gmail.com>
2022-05-15 04:02:52 -0700

    added test files

---
+++ test.js
@@ -0,0 +1,32 @@
+1.
+
+let firstName = 'adekunle'
+console.log(firstName.toUpperCase()) //ADEKUNLE//
+console.log(firstName.trim()) //adekunle//
+console.log(firstName.charAt(0)) //a//
+console.log(firstName.charAt(1))//d//
+console.log(firstName.charAt(2))//e//
+console.log(firstName.charAt(3))//k//
+
+let man = console.log(firstName.length)
+if(man<4){console.log('error')
+}else{
+    console.log('no error')
+}
+// no error//
+
+
+2.
+let man = new Date()
+
+console.log(man)
+//Sun May 15 2022 03:20:49 GMT-0700 (Pacific Daylight Time)//
+console.log(man.getDay())
+//0//
+console.log(man.getMonth())
+//4//
+console.log(man.getYear())
+//122//
+console.log(man.getFullYear())
+//2022//
+
